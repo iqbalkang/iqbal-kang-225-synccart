f48b9f47ca8f835c9b6efcded233dde6fdd10a7b
iqbal kang <iqbal-kang@iqbals-MacBook-Air.local>
2022-12-19 16:19:25 -0800

    first commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { BsFillCartFill } from 'react-icons/bs'
+import { IoLogIn } from 'react-icons/io5'
+
+const Navbar = () => {
+  return (
+    <nav className='bg-black text-white p-4'>
+      <div className='container mx-auto flex items-center'>
+        <Link to='/' className='text-2xl mr-auto'>
+          SyncCart
+        </Link>
+
+        <form>
+          <input
+            type='text'
+            placeholder='Search'
+            className='outline-none text-gray-600 py-1 px-2 mr-8 w-80 duration-200 focus:w-96'
+          />
+        </form>
+
+        <div className='flex gap-4'>
+          <div className='flex items-center gap-1 hover:text-red-500 duration-200 group'>
+            <BsFillCartFill />
+            <Link to='/cart'>Cart</Link>
+          </div>
+          <div className='flex items-center gap-1 hover:text-red-500 duration-200 group'>
+            <IoLogIn />
+            <Link to='/cart'>Login</Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
