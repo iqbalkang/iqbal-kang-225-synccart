053c4d00d88dc2937dc77cd9451175c72732b3ef
iqbal kang <iqbal-kang@iqbals-MacBook-Air.local>
2023-06-01 18:47:29 -0700

    changed axios base link

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const customFetch = axios.create({
-  baseURL: 'http://54.67.116.33/api/v1',
+  baseURL: 'http://13.57.222.128/api/v1',
 });
 
 export default customFetch;
