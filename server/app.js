ddd6df4423315db8f9ab248336f9c5a5dfb59f5b
iqbalkang225 <iqbal.kang.cohort225@gmail.com>
2024-06-25 13:39:07 -0700

    static files removed

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cors());
 
 // Production
-app.use(express.static(path.join(__dirname, '../client/build')));
+// app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use('/api/v1/products', productsRouter);
 app.use('/api/v1/users', usersRouter);
